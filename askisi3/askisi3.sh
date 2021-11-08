10bb8226adab6fbea677c0d68ea6f307b195f2c5
Zokas Filippos <zfilippos@csd.auth.gr>
2021-11-08 22:58:56 +0200

    Update askisi3.sh

--- askisi3/askisi3.sh
+++ askisi3/askisi3.sh
@@ -1,4 +1,7 @@
-read -p "Enter n number for words: " n
+#!/bin/bash
+truncate -s 0 file2.txt
+filename="$1"
+n="$2"
 count=0
 count1=0
 while read line; do
@@ -13,18 +16,20 @@ if [[ $line != "*** END OF THIS PROJECT GUTENBERG EBOOK ALICE IN WONDERLAND ***"
 else
 savecount1=$((count1+1)) 
 fi
-done < Alice.txt
-sed -n " ${savecount} , ${savecount1}p " Alice.txt > Alice1.txt
-sed -i ' 1d;$d ' Alice1.txt
+done < $filename
+sed -n " ${savecount} , ${savecount1}p " $filename > temp.txt
+sed -i ' 1d;$d ' temp.txt
 
 for word in $(
-  cat Alice1.txt | \
+  cat temp.txt | \
   tr '[:upper:]' '[:lower:]' | \
   tr -d '[:punct:]' | \
   tr -d  '/'
 )
 do
-  words=$(sed -e 's/[^[:alpha:]]/ /g' Alice1.txt | tr '\n' " " |  tr -s " " | tr " " '\n'| tr 'A-Z' 'a-z'  | sort | uniq -c | sort -nr | nl | head -n $n;)
-  echo "$words"
+  words=$(sed -e 's/[^[:alpha:]]/ /g' temp.txt | tr '\n' " " |  tr -s " "  | tr " " '\n'| tr 'A-Z' 'a-z'  | sort | uniq -c | sort -nr | nl | head -n $n;)
+  echo -e "$words\n" >> file2.txt
+	awk '{ print $3, $2}' file2.txt
+	rm file2.txt temp.txt 
 	exit
-done 
+done
